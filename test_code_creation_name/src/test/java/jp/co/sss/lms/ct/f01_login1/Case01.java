8c71baaff4f9f14d7b1fe725e9da2f1e2f647b6b
edu <edu@192.168.1.56>
2026-03-19 15:28:41 +0900

    add:Case01.java ログイン画面への遷移テストコード追加
    WebElementを利用してタイトルの値が期待値通りかどうか確認するテストを作成しました。
    期待値は以下の通り
    タイトル:ログイン | LMS
    
    Case01で取得したエビデンスをevidenceフォルダに追加しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -35,7 +36,12 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		goTo("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
