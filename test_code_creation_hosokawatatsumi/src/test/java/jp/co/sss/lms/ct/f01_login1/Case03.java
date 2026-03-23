49676372bd1de80cc645f90be782c1a795d871ae
edu <edu@B65DS.mshome.net>
2026-03-23 10:31:46 +0900

    add:Case03.java ログイン正常系のテストコード追加
    WebElementを利用してタイトルの値が期待値通りかどうか確認するテストを作成しました。
    期待値は以下の通り
    タイトル:コース詳細 ｜ LMS
    
    Case03で取得したエビデンスをevidenceフォルダに追加しました。

--- test_code_creation_hosokawatatsumi/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_hosokawatatsumi/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +37,23 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA01");
+		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 }
