1e1ba6f985413f5ddb4424849fee4438c68adb0e
edu <edu@B65DS.mshome.net>
2026-03-23 14:18:20 +0900

    add:Case04.java よくある質問への遷移のテストコード追加
    WebElementを利用してタイトルとタブ数の値が期待値通りかどうか確認するテストを作成しました。
    期待値は以下の通り
    タイトル:コース詳細 ｜ LMS
    タブ数:遷移前のタブ数+1
    
    Case04で取得したエビデンスをevidenceフォルダに追加しました。
    また、今後のテストを円滑に進めるため、WebDriverUtils.javaのcreateDriverメソッドに以下の処理を追加しました。
    ・Chromeのパスワードチェックを無効にする処理
    結合テスト仕様書の更新履歴を修正しました。

--- test_code_creation_hosokawatatsumi/src/test/java/jp/co/sss/lms/ct/util/WebDriverUtils.java
+++ test_code_creation_hosokawatatsumi/src/test/java/jp/co/sss/lms/ct/util/WebDriverUtils.java
@@ -3,6 +3,7 @@ package jp.co.sss.lms.ct.util;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -10,6 +11,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -30,9 +32,11 @@ public class WebDriverUtils {
 	 */
 	public static void createDriver() {
 		System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
-		webDriver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", Map.of("profile.password_manager_leak_detection", false));
+		webDriver = new ChromeDriver(options);
 	}
-	
+
 	/**
 	 * インスタンス終了
 	 */
@@ -48,7 +52,7 @@ public class WebDriverUtils {
 		webDriver.get(url);
 		pageLoadTimeout(5);
 	}
-	
+
 	/**
 	 * ページロードタイムアウト設定
 	 * @param second
@@ -56,7 +60,7 @@ public class WebDriverUtils {
 	public static void pageLoadTimeout(int second) {
 		webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(second));
 	}
-	
+
 	/**
 	 * 要素の可視性タイムアウト設定
 	 * @param locater
@@ -66,7 +70,7 @@ public class WebDriverUtils {
 		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(second));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(locater));
 	}
-	
+
 	/**
 	 * 指定ピクセル分だけスクロール
 	 * @param pixel
@@ -75,7 +79,6 @@ public class WebDriverUtils {
 		((JavascriptExecutor) webDriver).executeScript("window.scrollBy(0," + pixel + ");");
 	}
 
-	
 	/**
 	 * 指定位置までスクロール
 	 * @param pixel
