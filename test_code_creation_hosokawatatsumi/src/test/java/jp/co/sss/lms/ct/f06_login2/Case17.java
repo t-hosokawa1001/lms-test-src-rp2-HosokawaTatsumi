4c5ca5ba856985c4c88d8cd10809164e4333f5bb
edu <edu@B65DS.mshome.net>
2026-03-27 14:38:15 +0900

    add:Case17.java 受講生 初回ログイン 正常系のテストコード追加
    WebElementを利用して初回ログインの動作が期待値通りかどうか確認するテストを作成しました。
    期待値は以下の通り
    タイトル:コース詳細 ｜ LMS
    
    Case17で取得したエビデンスをevidenceフォルダに追加しました。

--- test_code_creation_hosokawatatsumi/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
+++ test_code_creation_hosokawatatsumi/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,28 +37,60 @@ public class Case17 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		goTo("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// 未ログインユーザー情報を入力してログインボタンを押下
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA02");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA02");
+		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+
+		assertEquals("セキュリティ規約 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
-		// TODO ここに追加
+
+		// 同意しますにチェックを入れ次へボタンを押下
+		webDriver.findElement(By.cssSelector("input[type='checkbox']")).click();
+		webDriver.findElement(By.xpath("//button[contains(text(), '次へ')]")).click();
+
+		visibilityTimeout(By.tagName("body"), 60);
+		assertEquals("パスワード変更 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 変更パスワードを入力し「変更」ボタン押下")
 	void test04() {
-		// TODO ここに追加
+
+		// 値を入力し変更ボタンを押下
+		webDriver.findElement(By.id("currentPassword")).sendKeys("StudentAA02");
+		webDriver.findElement(By.id("password")).sendKeys("PasswordAA02");
+		webDriver.findElement(By.id("passwordConfirm")).sendKeys("PasswordAA02");
+		webDriver.findElement(By.xpath("//button[contains(text(), '変更')]")).click();
+		visibilityTimeout(By.id("upd-btn"), 60);
+		webDriver.findElement(By.id("upd-btn")).click();
+
+		visibilityTimeout(By.tagName("body"), 60);
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 }
