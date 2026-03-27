b2380ea406a5b18330dafbcb6ba19ac18d87eb1a
edu <edu@B65DS.mshome.net>
2026-03-27 14:18:28 +0900

    add:Case15.java 受講生 初回ログイン 利用規約に不同意のテストコード追加
    WebElementを利用して利用規約同意画面の動作が期待値通りかどうか確認するテストを作成しました。
    期待値は以下の通り
    エラーメッセージ:セキュリティ規約への同意は必須です。
    
    Case15で取得したエビデンスをevidenceフォルダに追加しました。

--- test_code_creation_hosokawatatsumi/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
+++ test_code_creation_hosokawatatsumi/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,21 +37,41 @@ public class Case15 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		goTo("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// 未ログインユーザー情報を入力してログインボタンを押下
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA02");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA02");
+		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+
+		assertEquals("セキュリティ規約 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックをせず「次へ」ボタンを押下")
 	void test03() {
-		// TODO ここに追加
+
+		// 次へボタンを押下
+		webDriver.findElement(By.xpath("//button[contains(text(), '次へ')]")).click();
+
+		visibilityTimeout(By.tagName("body"), 60);
+		assertEquals("セキュリティ規約 | LMS", webDriver.getTitle());
+		assertEquals("セキュリティ規約への同意は必須です。", webDriver.findElement(By.className("error")).getText());
+		getEvidence(new Object() {
+		});
 	}
 
 }
